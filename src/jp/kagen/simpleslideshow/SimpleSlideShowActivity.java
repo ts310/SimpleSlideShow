988371d1241898c4e8d00733474676ccd073b4f7
Tsuyoshi Saito <tsuyoshi310@gmail.com>
2011-12-11 01:02:37 +0900

    Added spinner

--- src/jp/kagen/simpleslideshow/SimpleSlideShowActivity.java
+++ src/jp/kagen/simpleslideshow/SimpleSlideShowActivity.java
@@ -1,11 +1,16 @@
 package jp.kagen.simpleslideshow;
 
+import java.io.File;
+
 import android.app.Activity;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.os.Environment;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.ImageView;
+import android.widget.Spinner;
 
 public class SimpleSlideShowActivity extends Activity {
 	/** Called when the activity is first created. */
@@ -17,6 +22,22 @@ public class SimpleSlideShowActivity extends Activity {
 		// ImageView imageViewMain =
 		// (ImageView)findViewById(R.id.imageViewMain);
 		// imageViewMain.setImageResource(R.drawable.sample);
+
+		Spinner spinnerFile = (Spinner) findViewById(R.id.spinnerFile);
+		ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+				android.R.layout.simple_spinner_item);
+
+		File directory = Environment.getExternalStorageDirectory();
+
+		File[] files = directory.listFiles();
+
+		for (int i = 0; i < files.length; i++) {
+			if (files[i].getName().endsWith(".jpg")) {
+				adapter.add(files[i].getPath());
+			}
+		}
+
+		spinnerFile.setAdapter(adapter);
 	}
 
 	public void showImage(View view) {
